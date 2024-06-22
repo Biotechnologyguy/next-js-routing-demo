90b23ae1ce6549085784302a25855920cebd88cb
Shubham Thorat <shubhamvt121@gmail.com>
2024-06-22 21:05:36 +0530

    Nested Layouts

--- app/(e-commerce)/products/[id]/layout.tsx
+++ app/(e-commerce)/products/[id]/layout.tsx
@@ -1,18 +1,7 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      <div
-        style={{
-          padding: "20px",
-          backgroundColor: "grey",
-          color: "white",
-          marginBottom: "20px",
-          width: "100%",
-          textAlign: "center",
-        }}
-      >
-        <h1>Product Details</h1>
-      </div>
+      <h1>Product Details</h1>
       {children}
     </main>
   );
